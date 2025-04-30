62b9b33b1e3e4a027fc9b7531167ed22cd4131f1
A11enA <alasiakallen@gmail.com>
2025-04-29 19:54:13 -0500

    saving first slice of Pi

--- iot-projects/first-slice-of-pi/routes/sensors.js
+++ iot-projects/first-slice-of-pi/routes/sensors.js
@@ -2,6 +2,15 @@ const express = require('express'),
 	router = express.Router(),
 	resources = require('./../resources/model');
 
+	router.route("/").get(function (req, res, next) {
+		res.send(resources.pi.sensors);
+	  });
+	  
+	  router.route("/dht").get(function (req, res, next) {
+		res.send(resources.pi.sensors.dht);
+	  });
+
+
 
 
 module.exports = router;
--- iot-projects/first-slice-of-pi/servers/http.js
+++ iot-projects/first-slice-of-pi/servers/http.js
@@ -1,5 +1,20 @@
 const express = require('express'),
 	cors = require('cors');
-	
+	var app = express();
+	app.use(cors());
+
+	app.get("/", function (req, res) {
+		res.send('"Gottem" - Isagi Yoichi');
+	  });
+	app.get("/pi", function (req, res) {
+		res.send('"A violation of loyalty to Kaiser. That' + "'" + 's your first yellow card."-- Alexis Ness');
+	});
+	  
+	  
+
+
+
 
 module.exports = app;
+
+// I have looked through all files
