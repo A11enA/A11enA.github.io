4ec3d93916656719ac359f766b276dad96d90708
A11enA <alasiakallen@gmail.com>
2025-10-08 01:01:01 +0000

    Cald Distance base done

--- vgp-projects/calculate-distance/index.js
+++ vgp-projects/calculate-distance/index.js
@@ -1,5 +1,5 @@
 // TODO 4: Add a parameter for your gaming library in the index.js module:
-(function(window, createjs, opspark) {
+(function(window, createjs, opspark, librarby) {
   // OUR MODULE CODE GOES BELOW HERE //
 
   console.log('index.js initialized!');
@@ -20,10 +20,11 @@
   const engine = opspark.V6().activateResize();
   const canvas = engine.getCanvas();
   const stage = engine.getStage();
+  const calculateDistance = librarby.numz.calculateDistance
 
   
   // TODO 4.5: import your getDistance function here
-
+ console.log(librarby);
     
     
   const radius = 25; // the radius of our two circles
@@ -58,16 +59,20 @@
     /*
      * TODO 5: use getDistance to calculate the distance between shapeUp and 
      * the mouse. Store the result in a variable called distance:
-     */
+    */
     
-    
-    
+    var mouse = {
+      y: stage.mouseY,
+      x: stage.mouseX
+    }
+
+      var distance = calculateDistance(shapeUp, mouse)
     /*
      * TODO 6: Check if the mouse is within the area of shapeUp, and set the 
      * alpha property of shapeUp accordingly:
      */
-    
-    
+      distance > 25 ? shapeUp.alpha = 1 : shapeOver.alpha = 1
+      distance < 25 ? shapeUp.alpha = 0 : shapeOver.alpha = 0
     /*
      * Update the textfield with the current distance between the mouse and 
      *the edge of the shapeUp
@@ -94,4 +99,4 @@
   // OUR MODULE CODE GOES ABOVE HERE //
   
 // TODO 3: Pass your gaming library into the index.js module:
-}(window, window.createjs, window.opspark));
+}(window, window.createjs, window.opspark, window.myLibraryName));
