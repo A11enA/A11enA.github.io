b7737236a5457e205baa20636ee483ecc634c0a4
A11enA <alasiakallen@gmail.com>
2025-10-21 23:22:32 +0000

    init and link changes to match theme

---
+++ vgp-projects/calculate-rotation/bower_components/librarby/index.js
@@ -0,0 +1,24 @@
+(function(window, _) {
+  window.myLibraryName = window.myLibraryName || {
+    numz: {
+
+      calculateDistance(A, B){
+      var leg1 = Math.abs(B.x - A.x);
+      var leg2 = Math.abs(B.y - A.y);
+      var uns = (leg1**2) + (leg2**2);
+      var distance = uns ** (1/2);
+      return distance
+    },
+
+    degreesToRadians(degrees) {
+      return degrees * Math.Pi / 180;
+    },
+
+    radiansToDegrees(Radians) {
+      return radians * 180/Math.PI;
+    }
+
+    },
+    phyz: {},
+  };
+}(window, window._));
