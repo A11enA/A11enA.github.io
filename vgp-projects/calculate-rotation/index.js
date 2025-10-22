a4e9553440a6206f0bf3c2696244ca3c6f26767a
A11enA <alasiakallen@gmail.com>
2025-10-22 00:52:14 +0000

    fix dist

--- vgp-projects/calculate-rotation/index.js
+++ vgp-projects/calculate-rotation/index.js
@@ -1,31 +1,30 @@
 // TODO 4: add a param for your game lib last //
-(function(window, createjs, opspark, librarby) {
-  console.log('index.js initialized!');
+(function (window, createjs, opspark, librarby) {
+  console.log("index.js initialized!");
 
-  const
-    assets = opspark.assets,
+  const assets = opspark.assets,
     engine = opspark.V6().activateResize(),
     canvas = engine.getCanvas(),
     stage = engine.getStage(),
-    textfield = assets.makeTextfield('Degrees: ');
-  
+    textfield = assets.makeTextfield("Degrees: ");
+
   stage.addChild(textfield);
 
   // try a different hex color if you want //
-  const ship = assets.makeShip('#4286f4');
-  
-  
+  const ship = assets.makeShip("#ffaec7ff");
+
   // TODO 5: Center the ship on the stage //
-  
+  ship.x = canvas.width / 2;
+  ship.y = canvas.height / 2;
 
   // TODO 6: Add the ship to the stage //
-  
 
-  
+  stage.addChild(ship);
+
   function update(event) {
     /*
-     * TODO 7: Use your game lib's getAngleDegrees to get 
-     * the degrees of the angle between the ship and the 
+     * TODO 7: Use your game lib's getAngleDegrees to get
+     * the degrees of the angle between the ship and the
      * mouse position, and assign it to a const called
      * degrees.
      *
@@ -34,23 +33,24 @@
      * method takes two points. What do you need to do to translate
      * these values such that they're packed into a point?
      */
-    
-    
+    var mouse = {
+      y: stage.mouseY,
+      x: stage.mouseX,
+    };
+
+    const degrees = librarby.getAngleDegrees(ship, mouse);
+
     // TODO 8: Set the ship's rotation property to the degrees //
-    
-    
-    
+
     /*
-     * TODO 9: Uncomment the line below to update the textfield  
-     * with the current angle degrees. Degrees will be a value 
+     * TODO 9: Uncomment the line below to update the textfield
+     * with the current angle degrees. Degrees will be a value
      * between π and -π, or, 180 and -180.
      */
     // assets.updateText(textfield, `Degrees: ${degrees.toFixed(3)}°`, canvas);
   }
 
-  engine
-    .addTickHandlers(update)
-    .activateTick();
+  engine.addTickHandlers(update).activateTick();
 
-// TODO 3: pass your game lib last with, window.my-game-lib //
-}(window, window.createjs, window.opspark, window.librarby));
+  // TODO 3: pass your game lib last with, window.my-game-lib //
+})(window, window.createjs, window.opspark, window.librarby);
